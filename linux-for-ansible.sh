45aca4a4e0445ff9920e9c1677395fb625488a10
David Suart <davidsuart@users.noreply.github.com>
2017-10-11 13:09:30 +0100

    Improved readability

--- linux-for-ansible.sh
+++ linux-for-ansible.sh
@@ -19,9 +19,12 @@ if [ "$dist" == "Ubuntu" ]; then
   sudo apt-get install -y --no-install-recommends apt-transport-https ca-certificates software-properties-common
   sudo apt-add-repository -y ppa:ansible/ansible
   sudo apt-get update
-  sudo apt-get install -y --no-install-recommends python=2.7*
-  sudo apt-get install -y --no-install-recommends libpython-stdlib python-pkg-resources python-setuptools python-yaml python-httplib2 python-markupsafe python-jinja2 python-crypto python-six python-ecdsa python-paramiko
+  sudo apt-get install -y --no-install-recommends python=2.7* libpython2.7
+  sudo apt-get install -y --no-install-recommends libpython-stdlib python-pkg-resources python-setuptools python-six \
+                                                  python-httplib2 python-jinja2 python-markupsafe python-yaml \
+                                                  python-crypto python-ecdsa python-paramiko
   sudo apt-get install -y --no-install-recommends ansible sshpass
+  sudo apt-get install -y openssh-server
 
 else
 
